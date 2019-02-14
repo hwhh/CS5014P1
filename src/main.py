2eeb2d0b48eed3385c2a0e5b2c0a0d5858e9af90
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2019-02-14 17:08:59 +0000

    inital commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+# 1. load the data set
+# 2. clean the data set -- over fitting
+# 3. shuffle
+# 4. normalise the data
+# 4. create test / train split
+# 5. use Autoencoder to reduce dimensionality of data ?
+
+
+#
+
+
+def load_data():
+    df = pd.read_csv('../data/ENB2012_data.csv')
+    print()
+
+
+load_data()
